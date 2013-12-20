d64e981f2326fd056d84ac7d200c296d21a6e053
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-12-20 09:31:41 +0000

    Added implementation of reconstructing images from the principal components

--- code/PCA.py
+++ code/PCA.py
@@ -12,12 +12,14 @@ from os.path import isfile, join
 
 
 # The directory path to the images
-PICTURE_PATH = "pics/cambrdige_pics/s1"
+PICTURE_PATH = "/pics/cambrdige_pics/"
 
 # The current directory where the script is ran
 currentDir = os.path.dirname(os.path.abspath(__file__))
 
-
+"""
+  Converts the data to zero mean data.
+"""
 def convertDataToZeroMean(data):
   means = scipy.mean(data, axis=0)
 
@@ -31,10 +33,55 @@ def convertDataToZeroMean(data):
 
   return zeroMean
 
+
+"""
+  Uses a heuristic to evaluate how many dimensions should the data be reduced
+    to.
+
+Arguments:
+  eigenValues:
+    The eigen values of the covariance matrix, or numbers proportional to them.
+    Should be a numpy 1-D array.
+Returns:
+  The dimension the data should be reduced to.
+"""
+def dimensionFromEigenValues(eigenValues):
+  threshold = 0.01
+  dimension = 0
+
+  s = numpy.sum(eigenValues)
+  print "sum eigen" + str(s)
+
+  for eigen in eigenValues:
+    r = eigen / s
+    if r > threshold:
+      dimension += 1
+
+  return dimension
+
+# requires the eigen values to be sorted before
+def dimensionFromEigenValues(eigenValues):
+  threshold = 0.95
+  dimension = 0
+
+  s = numpy.sum(eigenValues)
+  print "sum eigen" + str(s)
+  current = 0
+  for eigen in eigenValues:
+    r = (eigen / s)
+    current += r
+    if current >= threshold:
+      break
+    dimension += 1
+
+  return dimension
+
+
 """
 This method uses the  Karhunen Lowe transform to fastly compute the
 eigen vaues of the data.
 
+The SVD method below should be preferred to this one.
 Arguments:
   train:
     Numpy array of arrays
@@ -72,6 +119,15 @@ def pca(train, dimension):
 
   index = 0
   result = []
+  if dimension == None:
+    # Get the eigen values
+    # Note that these are not the eigen values of the covariance matrix
+    # but the eigen values of U * U ^T
+    # however, this is fine because they just differ by a factor
+    # so the ratio between eigen values will be preserved
+    eigenValues = map(lambda x : x[0], sortedEigValsBigVecs)
+    dimension = dimensionFromEigenValues(eigenValues)
+
   for eigVal, vector in sortedEigValsBigVecs:
     if index >= dimension:
       break
@@ -106,8 +162,14 @@ def pcsWithSVD(train, dimension):
   # this means that the u's are already ordered as required, according
   # to the magnitute of the eigen values
   u, s, vh = scipy.linalg.svd(zeroMean)
-  return vh[0:dimension-1]
 
+  if dimension == None:
+    # Get the eigen values from the singular values
+    eigenValues = s ** 2;
+    dimension = dimensionFromEigenValues(eigenValues)
+    print "Using PCA dimension " + str(dimension)
+
+  return vh[0:dimension-1]
 
 """
 Arguments:
@@ -121,6 +183,7 @@ Returns:
 def transformVectorToImage(vec, size):
   return vec.reshape(size)
 
+
 """ Transforms the 2D images into 1D vectors
 Arguments:
   images: is a python list of numpy arrays
@@ -128,7 +191,7 @@ Returns:
   A python list of 1-D numpy arrays, transformed from the input 2D ones
   No data is lost in the transformation.
 """
-def trasformImageVectors(images):
+def transformImageToVector(images):
   return map(lambda x: x.reshape(-1), images)
 
 
@@ -144,10 +207,9 @@ Returns:
     The second element of the tuple if formed from the vector version of the
       eigen faces. This is kept for optimization reasons.
 """
-def getEigenFaces(pcaMethod, images, dimension):
-  imgs = map(lambda x: misc.imread(x, flatten=True), images)
-  imgSize = imgs[0].shape;
-  imgs = trasformImageVectors(imgs)
+def getEigenFaces(pcaMethod, images, dimension=None):
+  imgSize = images[0].shape;
+  imgs = transformImageToVector(images)
   imgs = scipy.array(imgs)
 
   vectors = pcaMethod(imgs, dimension)
@@ -155,18 +217,46 @@ def getEigenFaces(pcaMethod, images, dimension):
 
   return (eigenFaces, vectors)
 
+"""
+ Reduces a 2D image represented by a numpy 2D array of integer values(pixels)
+ to a lower dimension, dictated by the number of principal components.
+"""
+def reduceImageToLowerDimensions(principalComponents, image2D):
+  assert len(principalComponents > 0)
+
+  size = principalComponents[0].shape
+  vector = transformVectorToImage(image2D, size)
+
+  lowDimRepresentation = map(lambda x : x.T.dot(vector), principalComponents)
+  sameDimRepresentation = sum([ x * y for x, y in zip(principalComponents, lowDimRepresentation)])
+  return  (lowDimRepresentation, sameDimRepresentation)
+
+
 def main():
   # Load all the image files in the current directory
-  imagePath = os.path.join(currentDir, PICTURE_PATH)
-  # TODO: filter only img files
-  # TODO: get the images from the other folder as well
-  picFiles = [ os.path.join(PICTURE_PATH, f) for f in os.listdir(imagePath)
-               if os.path.isfile(os.path.join(imagePath,f)) ]
+  picFiles = []
+  path = currentDir + PICTURE_PATH
+  for root, dirs, files in os.walk(path):
+    if root != path:
+      picFiles += map(lambda x: os.path.join(root, x), files)
 
-  eigenFaces, vectors = getEigenFaces(pcsWithSVD, picFiles, 3)
-  plt.imshow(eigenFaces[0], cmap=plt.cm.gray)
+  print len(picFiles)
+
+  imgs = map(lambda x: misc.imread(x, flatten=True), picFiles)
+
+  eigenFaces, principalComponents = getEigenFaces(pcsWithSVD, imgs)
+  # plt.imshow(eigenFaces[0], cmap=plt.cm.gray)
+  # plt.show()
+
+  lowDimRepresentation, sameDimRepresentation = \
+      reduceImageToLowerDimensions(principalComponents, imgs[0])
+
+  plt.imshow(imgs[0], cmap=plt.cm.gray)
   plt.show()
 
+  image2D = transformVectorToImage(sameDimRepresentation, imgs[0].shape)
+  plt.imshow(image2D, cmap=plt.cm.gray)
+  plt.show()
   print "done"
 
 
