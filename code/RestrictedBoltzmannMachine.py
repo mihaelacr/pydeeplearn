dab4a9bd34798bf584d113d7e1fe24d147a0f8c4
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-12-26 11:11:36 +0000

    Added main and some prints + fixes. CD still not works

--- code/RestrictedBoltzmannMachine.py
+++ code/RestrictedBoltzmannMachine.py
@@ -18,8 +18,6 @@ import multiprocessing
 # Global multiprocessing pool, used for all updates in the networks
 pool = multiprocessing.Pool()
 
-
-
 """
  Represents a RBM
 """
@@ -36,19 +34,22 @@ class RBM(object):
     assert len(data) !=0
     self.nrHidden = nrHidden
     self.nrVisible = len(data[0])
-    self.weights = initializeWeights(nrVisible, nrHidden)
-    self.biases = intializeBiases(data, nrHidden)
+    self.data = data
+    self.trainingFunction = trainingFunction
+    self.weights = self.initializeWeights(self.nrVisible, self.nrHidden)
+    self.biases = self.intializeBiases(data, self.nrHidden)
+
 
   # you need to take a training algorithm as a parameter (CD, PCD)
   def train(self):
-    self.weights = trainingFunction(self.data, self.biases, self.weights)
+    self.biases, self.weights = self.trainingFunction(self.data, self.biases, self.weights)
 
   @classmethod
   def initializeWeights(cls, nrVisible, nrHidden):
     return np.random.normal(0, 0.01, (nrVisible, nrHidden))
 
   @classmethod
-  def initalizeBiases(cls, data, nrHidden):
+  def intializeBiases(cls, data, nrHidden):
     # get the procentage of data points that have the i'th unit on
     # and set the visible vias to log (p/(1-p))
     percentages = data.mean(axis=0, dtype='float') / len(data)
@@ -61,8 +62,9 @@ class RBM(object):
     return visibleBiases, hiddenBiases
 
 
-# think of adding this to the class
-# this might require some inheritance or things
+# TODO: add momentum to learning
+# TODO: different learning rates for weights and biases
+
 """ Training functions."""
 # Makes a step in the contrastiveDivergence algorithm
 # online or with mini-bathces?
@@ -70,46 +72,64 @@ class RBM(object):
 # It is importaant that the hidden values from the data are binary,
 # not probabilities
 def contrastiveDivergence(data, biases, weights, cdSteps=1):
-  # TODO: do something smarter
+  # TODO: do something smarter with the learning
   epsilon = 0.0001
-  for d in data:
+  # Check that it does rows in loops
+  for visible in data:
     # TODO: do CDn after some point
     # you can do it by calling the same function with the remaining data
-    # TODO: check if you have to use samples
-    hidden = updateLayer(Layer.HIDDEN, d, weights, True)
-    visibleReconstruction = updateLayer(Layer.VISIBLE, visible, weights, True)
-    hiddenReconstruction = updateLayer(Layer.HIDDEN, visible, weights, True)
-    weights = weights - epsilon * (np.outer(visible, hidden)
+    print "visible" + str(visible)
+    hidden = updateLayer(Layer.HIDDEN, visible, biases, weights, True)
+    visibleReconstruction = updateLayer(Layer.VISIBLE, hidden, biases, weights, False)
+    hiddenReconstruction = updateLayer(Layer.HIDDEN, visibleReconstruction, biases, weights, False)
+    weights = weights + epsilon * (np.outer(visible, hidden)
          - np.outer(visibleReconstruction - hiddenReconstruction))
 
-    # TODO: update the biases
-  return weights
+    # Update the visible biases
+    biases[0] += epsilon * (visible - visibleReconstruction)
+    # Update the hidden biases
+    biases[1] += epsilon * (hidden - hiddenReconstruction)
+  return biases, weights
+
 
 """ Updates an entire layer. This procedure can be used both in training
     and in testing.
 """
 def updateLayer(layer, otherLayerValues, biases, weightMatrix, binary=False):
-    bias = biases(layer)
+  bias = biases[layer]
 
-    def activation(x):
-      w = getWeights(layer, weightMatrix, x):
-      return activationProbability(activationSum(w, bias, otherLayerValues))
+  print "updating layer " + str(layer)
+  print "with bias" + str(bias)
 
-  probs = pool.map(activation, xrange(weightMatrix.shape(layer)))
+  print "weights" + str(weightMatrix.shape)
+  def activation(x):
+    w = weightVectorForNeuron(layer, weightMatrix, x)
+    print "weight vector" + str(w)
+    return activationProbability(activationSum(w, bias[x], otherLayerValues))
+
+  probs = map(activation, xrange(weightMatrix.shape[layer]))
+  probs = np.array(probs)
 
   if binary:
     # Sample from the distributions
     return sampleAll(probs)
 
-def getWeights(layer, weightMatrix, neuronNumber):
+  return probs
+
+
+def weightVectorForNeuron(layer, weightMatrix, neuronNumber):
   if layer == Layer.VISIBLE:
-    return weights[neuronNumber, :]
-  else layer == Layer.HIDDEN
-    return weights[:, neuronNumber]
+    return weightMatrix[neuronNumber, :]
+  # else layer == Layer.HIDDEN
+  return weightMatrix[:, neuronNumber]
 
 # TODO: check if you do it faster with matrix multiplication stuff
 # but hinton was adamant about the paralell thing
 def activationSum(weights, bias, otherLayerValues):
+  print "in activationSum"
+  print otherLayerValues
+  print weights
+
   return bias + np.dot(weights, otherLayerValues)
 
 """ Gets the activation sums for all the units in one layer.
@@ -137,11 +157,29 @@ def sample(p):
   return 0
 
 def sampleAll(probs):
-  vectorizedSample = np.vectorize(sample)
-  return vectorizedSample(probs)
+  return np.random.uniform(size=probs.shape) < probs
 
 def enum(**enums):
   return type('Enum', (), enums)
 
 # Create an enum for visible and hidden, for
 Layer = enum(VISIBLE=0, HIDDEN=1)
+
+
+"""Main. """
+def main():
+  X = np.array([[1,1,1,0,0,0], # Training subset
+                [1,0,1,0,0,0],
+                [1,1,1,0,0,0],
+                [0,0,1,1,1,0],
+                [0,0,1,1,0,0],
+                [0,0,1,1,1,0],
+
+                [0,0,1,1,1,1], # Validation subset
+                [1,1,1,0,0,0]])
+  rbm = RBM(X, 3, contrastiveDivergence)
+  rbm.train()
+
+
+if __name__ == '__main__':
+  main()
