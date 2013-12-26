0d720674b4c74f9522703dcd26b7119b1af01a8e
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-12-26 14:07:22 +0000

    Added common file

---
+++ code/common.py
@@ -0,0 +1,24 @@
+import numpy
+
+"""
+Arguments:
+  vec: A numpy 1-D vector.
+  size: A 2D tuple
+
+Returns:
+  A 2-D vector of dimension 'size', only if 'vec' has compatible dimensions.
+  Otherwise it throws an error.
+"""
+def transformVectorToImage(vec, size):
+  return vec.reshape(size)
+
+
+""" Transforms the 2D images into 1D vectors
+Arguments:
+  images: is a python list of numpy arrays
+Returns:
+  A python list of 1-D numpy arrays, transformed from the input 2D ones
+  No data is lost in the transformation.
+"""
+def transformImageToVector(images):
+  return map(lambda x: x.reshape(-1), images)
