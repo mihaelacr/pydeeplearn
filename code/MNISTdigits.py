90edfae60d0ce78a2cd9887d4dc00f2c5aed0a1c
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-12-26 14:15:39 +0000

    trying to train an RBM with mnist data. error

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -5,16 +5,23 @@ and the weights, to see what features we have learned"""
 
 import readmnist
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+
+import RestrictedBoltzmannMachine as RBM
 
 from common import *
 
 def main():
   images, labels = readmnist.read([2], dataset="training", path="MNIST")
-  plt.imshow(images[0], cmap=plt.cm.gray)
-  plt.show()
-  print "done"
+  # plt.imshow(images[0], cmap=plt.cm.gray)
+  # plt.show()
+  vectors = np.array(imagesToVectors(images))
 
+  rbm = RBM.RBM(vectors, 3, RBM.contrastiveDivergence)
+  rbm.train()
+
+
+  print "done"
 
 if __name__ == '__main__':
   main()
