c80670373b6820d547c3edfa3dcf088a2ebfaf92
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-12-28 13:38:52 +0000

    Added code for getting arguments for MNISTdigits and see how you train the network

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -3,9 +3,10 @@ on MNIST data and see how it behaves. It is not used for classification of
 handwritten digits, but rather as a way of visualizing the error of the RBM
 and the weights, to see what features we have learned"""
 
+# TODO: use cpikle instead of pikle
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-# TODO: use cpikle instead of pikle
 import pickle
 import readmnist
 import RestrictedBoltzmannMachine as RBM
@@ -13,44 +14,63 @@ import utils
 
 from common import *
 
-#
+NETWORK_FILE = "weights.p"
+
+# Get the arguments of the program
+parser = argparse.ArgumentParser(description='RBM for digit recognition')
+parser.add_argument('--save', type=bool,
+                    default=True, help="if true, the network is serialized and saved")
+parser.add_argument('--train', type=bool,
+                    default=True, help="if true, the network is trained from scratch from the traning data")
+args = parser.parse_args()
+
+
 def visualizeWeights(weights, imgShape, tileShape):
-  # return utils.tile_raster_images(weights, imgShape, tileShape, tile_spacing=(1, 1),scale_rows_to_unit_interval=False)
   return utils.tile_raster_images(weights, imgShape, tileShape, tile_spacing=(1, 1))
 
 def main():
+  trainImages, trainLabels = readmnist.read([2], dataset="training", path="MNIST")
+  testImages, testLabels = readmnist.read([2], dataset="testing", path="MNIST")
+  trainVectors = imagesToVectors(trainImages)
 
-  # t = pickle.load( open( "weights.p", "rb" ) )
+  # trainingScaledVectors = utils.scale_to_unit_interval(vectors)
+  trainingScaledVectors = trainVectors / 256
 
-  images, labels = readmnist.read([2], dataset="training", path="MNIST")
-  vectors = imagesToVectors(images)
+  testingVectors = imagesToVectors(testImages)
+  testingScaledVectors = testingVectors / 256
 
-  # scaledVecs = utils.scale_to_unit_interval(vectors)
-  scaledVecs = vectors / 256
-
-  # The number of hidden units is taken from a deep learning tutorial
   # Train the network
-  # The data are the values of the images have to be normalized before being
-  # presented to the network
-  rbm = RBM.RBM(scaledVecs, 500, RBM.contrastiveDivergence)
-  rbm.train()
+  if args.train:
+    # The number of hidden units is taken from a deep learning tutorial
+    # The data are the values of the images have to be normalized before being
+    # presented to the network
+    rbm = RBM.RBM(trainingScaledVectors, 500, RBM.contrastiveDivergence)
+    rbm.train()
+    t = visualizeWeights(rbm.weights.T, trainImages[0].shape, (10,10))
+  else:
+    # Take the saved network and use that for reconstructions
+    f = open(NETWORK_FILE, "rb")
+    t = pickle.load(f)
+    rbm = pickle.load(f)
 
-  recon = rbm.reconstruct(scaledVecs[0,:])
-  plt.imshow(vectorToImage(recon, images[0].shape), cmap=plt.cm.gray)
+
+  # Reconstruct a training image and see that it actually looks like a digit
+  recon = rbm.reconstruct(testingScaledVectors[0,:])
+  plt.imshow(vectorToImage(recon, trainImages[0].shape), cmap=plt.cm.gray)
   plt.show()
 
-  print rbm.weights.T
-  t = visualizeWeights(rbm.weights.T, images[0].shape, (10,10))
-  # TODO: add pickle behaviour to RBM
-  # pickle.dump(t, open( "weights.p", "wb" ) )
-  # pickle.dump(rbm.weights, open( "weights.p", "wb" ) )
-  # pickle.dump(rbm.biases, open( "weights.p", "wb" ) )
-
-  print t.sum()
-  print t.shape
+  # Show the weights and their form in a tile fashion
   plt.imshow(t, cmap=plt.cm.gray)
   plt.show()
   print "done"
+
+  # TODO: add pickle behaviour to RBM
+  # is this needd?
+  if args.save:
+    f = open("NETWORK_FILE.p", "wb")
+    pickle.dump(t, f)
+    pickle.dump(rbm, f)
+
 
 
 if __name__ == '__main__':
