4e7d8031baf7ada9543eac4470c0386f12ead367
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-01-04 17:23:25 +0000

    some prints

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -112,9 +112,10 @@ def deepbeliefMain():
   # TODO: think about what the network should do for 2 layers
   net.train(trainingScaledVectors, vectorLabels)
 
-  print "in classify"
-  print "actual class " +  str(testLabels[0])
-  print net.classify(trainVectors[0])
+  actual = testLabels[0:10]
+  found = net.classify(trainVectors[0:10])
+  for i in zip(actual, found):
+    print i
 
   ## Save network
   f = open("deepbelief.p", "wb")
