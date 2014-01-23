7cda545eadb4e7e6311d03ae3978b59c1490c63d
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-01-23 20:52:16 +0000

    trying to immplement the dot product in GPU

--- code/test.py
+++ code/test.py
@@ -1,33 +1,35 @@
 # this file is made to see how theano works and the speedup
 # it gives you on GPU versus a normal implementation on CPU (ran on my computer)
 
+import theano
 import theano.tensor as T
-from theano import function
+from theano import function, shared
 import numpy as np
 
 
 import time
-x = T.matrix('x', dtype='int64')
-y = T.matrix('y', dtype='int64')
+x = T.matrix('x', dtype=theano.config.floatX)
+y = T.matrix('y', dtype=theano.config.floatX)
+
 # z = x + y
 # f = function([x, y], z)
+# sc = shared(np.tile(0, 30, 30, dtype=theano.config.floatX), name='sc')
+sc = shared(np.zeros((10, 10), dtype = theano.config.floatX), name='sc')
 
-
-sc = T.dot(x, y)
-mydot = function([x,y], sc)
+# sc = T.dot(x, y)
+mydot = function( [x,y], updates=( (sc, T.dot(x,y)), ))
 
 # a = np.random.random_integers(0, 100, (1000, 1000))
 # b = np.random.random_integers(0, 100, (1000, 1000))
 
-a = np.tile(40, (1000, 1000))
-b = np.tile(23, (1000, 1000))
+# We need to declare the variables shared to run on GPU
+a = np.tile(40.0, (1000, 1000))
+b = np.tile(23.0, (1000, 1000))
 print "go"
 
 before = time.time()
-
 c = mydot(a,b)
+print c.sum()
 print time.time() - before
-
-print c
 
 # print f(np.array([[2,3]]), np.array([[4,5]]))
