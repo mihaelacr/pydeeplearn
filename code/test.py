312a9c1d8a8e96619c8656f551a4755b14eb5085
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-01-23 16:42:08 +0000

    More

--- code/test.py
+++ code/test.py
@@ -5,8 +5,10 @@ import theano.tensor as T
 from theano import function
 import numpy as np
 
-x = T.matrix('x')
-y = T.matrix('y')
+
+import time
+x = T.matrix('x', dtype='int64')
+y = T.matrix('y', dtype='int64')
 # z = x + y
 # f = function([x, y], z)
 
@@ -14,7 +16,18 @@ y = T.matrix('y')
 sc = T.dot(x, y)
 mydot = function([x,y], sc)
 
-a = np.random.random_integers(0, 100, (10000, 10000))
-b = np.random.random_integers(0, 100, (10000, 10000))
-print mydot(a,b)
+# a = np.random.random_integers(0, 100, (1000, 1000))
+# b = np.random.random_integers(0, 100, (1000, 1000))
+
+a = np.tile(40, (1000, 1000))
+b = np.tile(23, (1000, 1000))
+print "go"
+
+before = time.time()
+
+c = mydot(a,b)
+print time.time() - before
+
+print c
+
 # print f(np.array([[2,3]]), np.array([[4,5]]))
