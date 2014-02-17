86efadc86a499387b04eced146b728fc3379d9bc
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-02-17 16:25:19 +0000

    removed commented out code

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -140,32 +140,6 @@ class DBN(object):
     sharedLabels = T.cast(theano.shared(np.asarray(labels,
                                                dtype=theano.config.floatX)),
                           'int32')
-    # def shared_dataset(data_xy, borrow=True):
-    #     """ Function that loads the dataset into shared variables
-
-    #     The reason we store our dataset in shared variables is to allow
-    #     Theano to copy it into the GPU memory (when code is run on GPU).
-    #     Since copying data into the GPU is slow, copying a minibatch everytime
-    #     is needed (the default behaviour if the data is not in a shared
-    #     variable) would lead to a large decrease in performance.
-    #     """
-    #     data_x, data_y = data_xy
-    #     shared_x = theano.shared(numpy.asarray(data_x,
-    #                                            dtype=theano.config.floatX),
-    #                              borrow=borrow)
-    #     shared_y = theano.shared(numpy.asarray(data_y,
-    #                                            dtype=theano.config.floatX),
-    #                              borrow=borrow)
-    #     # When storing data on the GPU it has to be stored as floats
-    #     # therefore we will store the labels as ``floatX`` as well
-    #     # (``shared_y`` does exactly that). But during our computations
-    #     # we need them as ints (we use labels as index, and if they are
-    #     # floats it doesn't make sense) therefore instead of returning
-    #     # ``shared_y`` we will have to cast it to int. This little hack
-    #     # lets ous get around this issue
-    #     return shared_x, T.cast(shared_y, 'int32')
-
-
     learningRate = 0.1
     batchLearningRate = learningRate / self.miniBatchSize
 
