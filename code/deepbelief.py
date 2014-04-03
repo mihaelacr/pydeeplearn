de63854607c3297ba031f8fc2c1aa0a925baec9a
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-03 10:21:13 +0100

    fixed the shape of the sampling

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -107,7 +107,7 @@ class MiniBatchTrainer(object):
       # the results with softmax and regression layers?
       if stage != len(self.weights) -1:
         dropout_mask = self.theano_rng.binomial(n=1, p=hiddenDropout,
-                                            size=self.input.shape,
+                                            size=self.linearSum.shape,
                                             dtype=theanoFloat)
         currentLayerValues = dropout_mask * T.nnet.sigmoid(linearSum)
       else:
