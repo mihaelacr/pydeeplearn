08859838c085630f7696175cb2b2f339be588272
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-04 21:42:05 +0100

    4 count

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -324,7 +324,7 @@ class DBN(object):
     lastValidationError = np.inf
     count = 0
     epoch = 0
-    while epoch < maxEpochs and count < 3:
+    while epoch < maxEpochs and count < 4:
       print "epoch"
 
       for batchNr in xrange(nrMiniBatches):
