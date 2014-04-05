b581c11db6c883dad8961c346a093e0103dfdec3
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-05 10:09:25 +0100

    removed some part that had to do with the emotions

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -10,9 +10,6 @@ import restrictedBoltzmannMachine as rbm
 import deepbelief as db
 import utils
 import PCA
-import glob
-
-import DimensionalityReduction
 
 from common import *
 
@@ -188,49 +185,6 @@ def deepbeliefMNIST():
     f = open(args.netFile, "wb")
     pickle.dump(net, f)
     f.close()
-
-"""
-  Arguments:
-    big: should the big or small images be used?
-    folds: which folds should be used (1,..5) (a list). If None is passed all
-    folds are used
-"""
-def deepBeliefKanade(big=False, folds=None):
-  if big:
-    files = glob.glob('kanade_150*.pickle')
-  else:
-    files = glob.glob('kanade_f*.pickle')
-
-  if not folds:
-    folds = range(1, 6)
-
-  # Read the data from them. Sort out the files that do not have
-  # the folds that we want
-  # TODO: do this better (with regex in the file name)
-  # DO not reply on the order returned
-  files = files[folds]
-
-  data = []
-  labels = []
-  for filename in files:
-    with open(filename, "rb") as  f:
-      # Sort out the labels from the data
-      dataAndLabels = pickle.load(f)
-      foldData = dataAndLabels[0:-1 ,:]
-      foldLabels = dataAndLabels[-1,:]
-      data.append(foldData)
-      labels.append(foldLabels)
-
-  # Do LDA
-
-  # Create the network
-
-  # Test
-
-  # You can also group the emotions into positive and negative to see
-  # if you can get better results (probably yes)
-  pass
-
 
 # TODO: fix this (look at the ML coursework for it)
 # Even better, use LDA
