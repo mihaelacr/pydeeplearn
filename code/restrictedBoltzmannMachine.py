ac827c992165d983ef2d480662e9dec56807794f
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-07 20:25:07 +0100

    fixing errors caused by last change

--- code/restrictedBoltzmannMachine.py
+++ code/restrictedBoltzmannMachine.py
@@ -52,16 +52,16 @@ class RBMMiniBatchTrainer(object):
     # TODO: scan for a loop in theano
     # Now do 1 step in CD, but later you will have to do more
     # I think that should happen with scan or something
-    hiddenActivations = T.nnet.sigmoid(T.dot(input, w) + biasHidden)
+    hiddenActivations = T.nnet.sigmoid(T.dot(input, self.weights) + self.biasHidden)
     self.hidden = self.theano_rng.binomial(size=hiddenActivations.shape,
                                           n=1, p=hiddenActivations,
                                           dtype=theanoFloat)
 
-    visibleRec = T.nnet.sigmoid(T.dot(self.hidden, w.T) + biasVisible)
+    visibleRec = T.nnet.sigmoid(T.dot(self.hidden, self.weights.T) + self.biasVisible)
     self.visibleReconstruction = self.theano_rng.binomial(size=visibleRec.shape,
                                           n=1, p=visibleRec,
                                           dtype=theanoFloat)
-    hiddenRec = T.nnet.sigmoid(T.dot(self.visibleReconstruction, w) + biasHidden)
+    hiddenRec = T.nnet.sigmoid(T.dot(self.visibleReconstruction, self.weights) + self.biasHidden)
     self.hiddenReconstruction = self.theano_rng.binomial(size=hiddenRec.shape,
                                           n=1, p=hiddenRec,
                                           dtype=theanoFloat)
