01a5ce320fcb75b68bb168fd2483c8a5ecba545f
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-10 14:08:01 +0100

    back to 95 for checks

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -337,7 +337,7 @@ class DBN(object):
       if epoch < maxEpochs / 10:
         momentum = np.float32(0.5)
       else:
-        momentum = np.float32(0.98)
+        momentum = np.float32(0.95)
 
       for batchNr in xrange(nrMiniBatches):
         trainModel(batchNr, momentum)
