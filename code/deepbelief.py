04e324e3397a1678bc8b95b8bc05919271633d04
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-21 16:35:41 +0100

    changed the assertion as well

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -413,7 +413,7 @@ class DBN(object):
 
       for batchNr in xrange(self.nrMiniBatches):
         trainModel(batchNr, momentum)
-        for i in xrange(self.nrLayers - 1):
+        for i in xrange(self.nrLayers - 2):
           assert np.all(np.linalg.norm(batchTrainer.weights[i].get_value(), axis=0) <= self.normConstraint + 1e-8)
 
 
