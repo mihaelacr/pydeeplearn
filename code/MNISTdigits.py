c0262cbe3e1bbc650598bc24b4cd36e7646db743
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-23 23:47:10 +0100

    swapping parameters

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -176,15 +176,14 @@ def cvMNIST():
 
   vectorLabels = labelsToVectors(trainLabels, 10)
 
-
   permutation = np.random.permutation(range(training))
   bestFold = -1
   bestError = np.inf
 
   if args.relu:
-    params =[(0.1, 0.1) , (0.1, 0.05), (0.05, 0.1), (0.05, 0.05)]
-  else:
     params =[(0.01, 0.01) , (0.01, 0.05), (0.05, 0.1), (0.05, 0.05)]
+  else:
+    params =[(0.1, 0.1) , (0.1, 0.05), (0.05, 0.1), (0.05, 0.05)]
 
   nrFolds = len(params)
   foldSize = training / nrFolds
