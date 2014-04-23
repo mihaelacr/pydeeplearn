ce839b4054c360398005a5cb560098fec7bf3e05
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-23 13:13:45 +0100

    removed c

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -332,7 +332,7 @@ class DBN(object):
     # the error is the sum of the errors in the individual cases
     # t = T.as_tensor_variable(batchTrainer.weights)
     # comps, updates_ = theano.map(fn=lambda x: T.sum(T.abs_(x)), sequences=[t])
-    error = T.sum(batchTrainer.cost(y)) + c
+    error = T.sum(batchTrainer.cost(y))
     for w in batchTrainer.weights:
       error+= T.abs_(w)
 
