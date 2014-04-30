de7f71e74caad215e354bd7fdcc8f7d8f753777c
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-01 00:31:54 +0100

    print

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -822,7 +822,7 @@ def main():
   np.random.seed(6)
   if args.db + args.pca + args.rbm + args.cv +\
       args.ann + args.cvgauss + args.rbmGauss + args.dbgauss + args.display != 1:
-    raise Exception("You decide on one main method to run")
+    raise Exception("You have to decide on one main method to run")
 
   # makeNicePlots()
   # makeMNISTpic()
