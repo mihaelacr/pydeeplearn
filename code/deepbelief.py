67e992f4cc74560dc818e3f59276d5e9cc989a58
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-03 10:48:24 +0100

    no plot when there are fixed updates until I fix the matplotlib issue

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -491,7 +491,7 @@ class DBN(object):
       print "you have interrupted training"
       print "we will continue testing with the state of the network as it is"
 
-    plotTraningError(trainingErrors)
+    # plotTraningError(trainingErrors)
 
     print "number of epochs"
     print epoch
