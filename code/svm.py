2a8fcb863fcdc784d51f276fe86fdcbf7fd56b45
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-03 15:42:26 +0100

    using the last element of the hidden activations to feed into the svm

--- code/svm.py
+++ code/svm.py
@@ -13,23 +13,23 @@ def svmOnHiddenActivations(dbnNet, train, test, trainLabels, testLabels):
 
   # print getClassificationError(predicted, testLabels)
 
-  trainHiddenRepresentations = dbnNet.getHiddenActivations(train)
+  trainHiddenRepresentations = dbnNet.getHiddenActivations(train)[-1]
 
   trainHiddenRepresentations = scale(trainHiddenRepresentations)
   classifier.fit(trainHiddenRepresentations, trainLabels)
 
   testHiddenRepresentation = scale(testHiddenRepresentation)
-  testHiddenRepresentation = dbnNet.getHiddenActivations(test)
+  testHiddenRepresentation = dbnNet.getHiddenActivations(test)[-1]
   predicted = classifier.predict(testHiddenRepresentation)
 
   print getClassificationError(predicted, testLabels)
 
 
 def SVMCV(dbnNet, train, trainLabels, test, testLabels):
-  trainHiddenRepresentations = dbnNet.getHiddenActivations(train)
+  trainHiddenRepresentations = dbnNet.getHiddenActivations(train)[-1]
   trainHiddenRepresentations = scale(trainHiddenRepresentations)
 
-  testHiddenRepresentation = dbnNet.getHiddenActivations(test)
+  testHiddenRepresentation = dbnNet.getHiddenActivations(test)[-1]
   testHiddenRepresentation = scale(testHiddenRepresentation)
 
   parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
