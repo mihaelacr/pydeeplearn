c9786dbbb6e1e23a492fd5dac027a622f1a6955d
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-08 13:13:55 +0100

    massive refactoring in the similarities code

--- code/similarity.py
+++ code/similarity.py
@@ -1,26 +1,17 @@
 """ This module implements the idea of finding out emotions similarities
 by using the experiments similar to what Hinton describes in his NRelu paper."""
 
-
-import argparse
 import restrictedBoltzmannMachine as rbm
 
 import numpy as np
 import theano
 from theano import tensor as T
-from sklearn import cross_validation
 
 from common import *
-from readfacedatabases import *
+from similarities_utils import *
 
 theanoFloat  = theano.config.floatX
 
-
-parser = argparse.ArgumentParser(description='digit recognition')
-parser.add_argument('--relu', dest='relu',action='store_true', default=False,
-                    help=("if true, trains the RBM or DBN with a rectified linear unit"))
-
-args = parser.parse_args()
 
 class Trainer(object):
 
@@ -56,15 +47,25 @@ class SimilarityNet(object):
 
   # TODO: add sizes and activation functions here as well
   # plus rbm learning rates
-  def __init__(self, learningRate):
+  def __init__(self, learningRate, rbmNrVis, rbmNrHid, rbmLearningRate,
+                rbmDropoutVis, rbmDropoutHid, binary):
     self.learningRate = learningRate
+    self.binary = binary
+    self.rbmNrVis = rbmNrVis
+    self.rbmNrHid = rbmNrHid
+    self.rbmLearningRate = rbmLearningRate
+    self.rbmDropoutHid = rbmDropoutVis
+
 
   def _trainRBM(self, data1, data2):
     data = np.vstack([data1, data2])
+    # TODO: activation function change
     activationFunction = T.nnet.sigmoid
 
-    net = rbm.RBM(1200, 500, 0.001, 1, 1,
-                    binary=1-args.relu,
+    net = rbm.RBM(self.rbmNrVis, self.rbmNrHid, self.rbmLearningRate,
+                    hiddenDropout=self.rbmDropoutHid,
+                    visibleDropout=self.rbmDropoutVis,
+                    binary=self.binary,
                     visibleActivationFunction=activationFunction,
                     hiddenActivationFunction=activationFunction,
                     rmsprop=True,
@@ -115,7 +116,7 @@ class SimilarityNet(object):
 
     for epoch in xrange(epochs):
       for miniBatch in xrange(nrMiniBatches):
-        bla = discriminativeTraining(miniBatch)
+        discriminativeTraining(miniBatch)
 
   def test(self, testData1, testData2):
     # If it is too slow try adding mini batches
@@ -150,149 +151,8 @@ def cosineDistance(first, second):
   normSecond = T.sum(T.sqrt(second), axis=1)
   return T.sum(first * second, axis=1) / (normFirst * normSecond)
 
-# you can create more tuples than just one per image
-# you can put each image in 5 tuples and that will probably owrk better
-# it might be useful to also give the same image twice
-def splitData(imgsPerSubject=None):
-  subjectsToImgs = readMultiPIESubjects()
-
-  data1 = []
-
-  data2 = []
-
-  shuffling = []
-  subjectsShuffling = []
-  subjects1 = []
-  subjects2 = []
-
-  for subject, images in subjectsToImgs.iteritems():
-    if imgsPerSubject is not None:
-      images = images[:imgsPerSubject]
-
-    lastIndex = len(images)/ 4 + subject % 2
-    delta = len(images)/ 4 + (subject + 1) % 2
-    last2Index = lastIndex + delta
-    data1 += images[0: lastIndex]
-    data2 += images[lastIndex: last2Index]
-
-    subjects1 += [subject] * lastIndex
-    subjects2 += [subject] * delta
-
-    imagesForShuffling = images[last2Index : ]
-    shuffling += imagesForShuffling
-    subjectsShuffling += [subject] * len(imagesForShuffling)
-
-  print "len(subjectsShuffling)"
-  print len(subjectsShuffling)
-
-  print "shuffling"
-  print len(shuffling)
-
-  assert len(shuffling) == len(subjectsShuffling)
-  shuffling, subjectsShuffling = shuffleList(shuffling, subjectsShuffling)
-
-  print len(data1)
-  print len(data2)
-  # Warning: hack
-  data2 = data2[:-1]
-  subjects2 = subjects2[:-1]
-
-  data1 = np.array(data1)
-  data2 = np.array(data2)
-  subjects1 = np.array(subjects1)
-  subjects2 = np.array(subjects2)
-
-  trainData1, testData1, trainData2, testData2, trainSubjects1, testSubjects1,\
-        trainSubjects2, testSubjects2 = splitTrainTest(data1, data2, subjects1, subjects2, 5)
-
-  print "trainData1.shape"
-  print trainData1.shape
-
-  shuffledData1 = shuffling[0: len(shuffling) / 2]
-  shuffledData2 = shuffling[len(shuffling)/2 :]
-
-  subjectsData1 = subjectsShuffling[0: len(shuffling) /2]
-  subjectsData2 = subjectsShuffling[len(shuffling)/2:]
-
-  shuffledData2 = shuffledData2[:-1]
-  subjectsData2 = subjectsData2[:-1]
-
-  shuffledData1 = np.array(shuffledData1)
-  shuffledData2 = np.array(shuffledData2)
-  subjectsData1 = np.array(subjectsData1)
-  subjectsData2 = np.array(subjectsData2)
-
-  print len(shuffledData1)
-  print len(shuffledData2)
-
-  trainShuffedData1, testShuffedData1, trainShuffedData2, testShuffedData2,\
-    trainShuffledSubjects1, testShuffledSubjects1, trainShuffledSubjects2, testShuffledSubjects2 =\
-        splitTrainTest(shuffledData1, shuffledData2,
-                      subjectsData1, subjectsData2, 5)
-
-  trainData1 = np.vstack((trainData1, trainShuffedData1))
-
-  print "trainData1.shape"
-  print trainData1.shape
-
-  trainData2 = np.vstack((trainData2, trainShuffedData2))
-
-  testData1 = np.vstack((testData1, testShuffedData1))
-  testData2 = np.vstack((testData2, testShuffedData2))
-
-  trainSubjects1 = np.hstack((trainSubjects1, trainShuffledSubjects1))
-  testSubjects1 = np.hstack((testSubjects1, testShuffledSubjects1))
-
-  trainSubjects2 = np.hstack((trainSubjects2, trainShuffledSubjects2))
-  testSubjects2 = np.hstack((testSubjects2, testShuffledSubjects2))
-
-  assert len(subjects1) == len(subjects2)
-  assert len(trainSubjects1) == len(trainSubjects1)
-  assert len(testSubjects1) == len(testSubjects2)
-  similaritiesTrain = (trainSubjects1 == trainSubjects2)
-  similaritiesTest = (testSubjects1 == testSubjects2)
-
-
-  print "trainSubjects1.shape"
-  print trainSubjects1.shape
-
-  print "similaritiesTrain.shape"
-  print similaritiesTrain.shape
-  print similaritiesTrain
-
-  assert len(trainData1) == len(trainData2)
-  assert len(testData1) == len(testData2)
-
-  return trainData1, trainData2, testData1, testData2, similaritiesTrain, similaritiesTest
-
 # Here  you need different measures than 0, 1 according to what you want it to learn
 # for the emotions part
 def defineSimilartyMesures():
   None
 
-# TODO: move to common?
-def splitTrainTest(data1, data2, labels1, labels2, ratio):
-  assert len(data1) == len(data2)
-  assert len(labels1) == len(labels2)
-  assert len(labels1) == len(data1)
-  # Random data for training and testing
-  kf = cross_validation.KFold(n=len(data1), k=ratio)
-  for train, test in kf:
-    break
-
-  return (data1[train], data1[test], data2[train], data2[test],
-          labels1[train], labels1[test], labels2[train], labels2[test])
-
-def main():
-  trainData1, trainData2, testData1, testData2, similaritiesTrain, similaritiesTest = splitData(10)
-
-  simNet = SimilarityNet(0.01)
-  simNet.train(trainData1, trainData2, similaritiesTrain)
-
-  res = simNet.test(testData1, testData2)
-  print res
-
-
-
-if __name__ == '__main__':
-  main()
---
+++ code/similarityMain.py
@@ -0,0 +1,31 @@
+import argparse
+
+from similarities_utils import *
+import similarity
+
+parser = argparse.ArgumentParser(description='digit recognition')
+parser.add_argument('--relu', dest='relu',action='store_true', default=False,
+                    help=("if true, trains the RBM or DBN with a rectified linear unit"))
+
+args = parser.parse_args()
+
+
+def main():
+  trainData1, trainData2, testData1, testData2, similaritiesTrain, similaritiesTest = splitData(10)
+
+  simNet = similarity.SimilarityNet(0.01, learningRate=learningRate,
+                                    binary=True,
+                                    rbmNrVis=1200,
+                                    rbmNrHid=500,
+                                    rbmLearningRate=0.001,
+                                    rbmDropoutHid=1.0,
+                                    rbmDropoutVis=1.0)
+
+  simNet.train(trainData1, trainData2, similaritiesTrain)
+
+  res = simNet.test(testData1, testData2)
+  print res
+
+
+if __name__ == '__main__':
+  main()
