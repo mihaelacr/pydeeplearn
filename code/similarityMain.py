8b315dbd2165793f21330ca2a75db2b29b94e639
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-08 22:52:10 +0100

    cv in similarity

--- code/similarityMain.py
+++ code/similarityMain.py
@@ -1,4 +1,5 @@
 import argparse
+from sklearn import cross_validation
 
 from similarity_utils import *
 import similarity
@@ -34,7 +35,29 @@ def similarityMain():
 
 
 def similarityCV():
-  pass
+  trainData1, trainData2, testData1, testData2, similaritiesTrain, similaritiesTest = splitData()
+
+  params = [(0.1, 0.001), (0.1, 0.005), (0.01, 0.001), (0.01, 0.005)]
+  kf = cross_validation.KFold(n=len(trainData1), k=len(params))
+
+  for train, test in kf:
+    simNet = similarity.SimilarityNet(learningRate=params[0],
+                                    maxMomentum=0.95,
+                                    binary=True,
+                                    rbmNrVis=1200,
+                                    rbmNrHid=500,
+                                    rbmLearningRate=params[1],
+                                    rbmDropoutHid=1.0,
+                                    rbmDropoutVis=1.0)
+
+    simNet.train(trainData1, trainData2, similaritiesTrain)
+
+    res = simNet.test(testData1, testData2)
+
+    error = (similaritiesTest - res)  * 1.0 / len(res)
+
+    print res
+    print error
 
 
 def main():
