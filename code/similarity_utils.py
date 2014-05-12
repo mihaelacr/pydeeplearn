00411ec0891cc24fed7cb1d3aaf45e751fbe44be
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-12 20:26:19 +0100

    Fix var name

--- code/similarity_utils.py
+++ code/similarity_utils.py
@@ -45,7 +45,7 @@ def splitShuffling(shuffling, labelsShuffling):
     print len(shuffling)
 
     label1 = np.random.choice(currentLabels, 1, replace=False)
-    label2 = np.random.choice(list(currentLabels).remove(label2), 1, replace=False)
+    label2 = np.random.choice(list(currentLabels).remove(label1), 1, replace=False)
 
     print label1
     print label2
