c64dee20961671b4b78a21d32d136d5c2f5c0e16
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-15 13:35:08 +0100

    new main for read face databases

--- code/readfacedatabases.py
+++ code/readfacedatabases.py
@@ -469,5 +469,5 @@ def readAberdeen(detectFaces, equalize):
 if __name__ == '__main__':
   # path = '/home/aela/uni/project/Multi-PIE_Aligned/A_MultiPIE.mat'
   # readMultiPIE(show=True)
-  readCroppedYaleSubjects(show=True)
-
+  # readCroppedYaleSubjects(show=True)
+  makeMultiPieImagesForReport()
