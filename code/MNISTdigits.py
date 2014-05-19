b9514fafdbf21ff56ace688d1eed4c8dd7d992cf
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-19 21:18:11 +0100

    Stop using the sparsity constraints

--- code/MNISTdigits.py
+++ code/MNISTdigits.py
@@ -755,7 +755,7 @@ def deepbeliefMNISTGaussian():
   supervisedLearningRate = 0.005
   momentumMax = 0.95
   sparsityTragetRbm = 0.1
-  sparsityConstraintRbm = True
+  sparsityConstraintRbm = False
   sparsityRegularizationRbm = 0.1
 
   if args.train:
