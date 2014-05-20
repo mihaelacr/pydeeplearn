9c5cdc71ac73197bbbdab4043c54a891d949ad4b
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-20 20:24:36 +0100

    added code in maxout which splits the data into train, test and validate

--- code/maxoutMain.py
+++ code/maxoutMain.py
@@ -13,10 +13,15 @@ from pylearn2.utils import serial
 from pylearn2.costs.mlp.dropout import Dropout
 from theano import function
 from theano import tensor as T
+from sklearn import cross_validation
+from sklearn.metrics import confusion_matrix
 
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 
 import readfacedatabases
+from common import *
+
+
 
 class MultiPIE(DenseDesignMatrix):
 
@@ -90,16 +95,17 @@ def MNISTmain():
 def MultiPIE():
   h0 = maxout.Maxout(layer_name='h0', num_units=1200, num_pieces=2, W_lr_scale=1.0, irange=0.005, b_lr_scale=1.0)
   h1 = maxout.Maxout(layer_name='h1', num_units=1500, num_pieces=2, W_lr_scale=1.0, irange=0.005, b_lr_scale=1.0)
-  h2 = maxout.Maxout(layer_name='h1', num_units=1500, num_pieces=2, W_lr_scale=1.0, irange=0.005, b_lr_scale=1.0)
+  h2 = maxout.Maxout(layer_name='h2', num_units=1500, num_pieces=2, W_lr_scale=1.0, irange=0.005, b_lr_scale=1.0)
   outlayer = mlp.Softmax(layer_name='y', n_classes=6, irange=0)
 
   layers = [h0, h1, h2, outlayer]
 
   model = mlp.MLP(layers, nvis=1200)
 
-  train = MultiPIE('train', start=0, stop=50000)
-  valid = MultiPIE('valid', start=50000, stop=60000)
-  test = MultiPIE('test',   start=0, stop=10000)
+  trainIndices, validationIndices, testIndices = getMultiPIEindices()
+  train = MultiPIE('train', indices=trainIndices)
+  valid = MultiPIE('valid', indices=validationIndices)
+  test = MultiPIE('test',   indices=testIndices)
 
   monitoring = dict(valid=valid)
   termination = MonitorBased(channel_name="valid_y_misclass", N=100)
@@ -109,7 +115,7 @@ def MultiPIE():
   algorithm = sgd.SGD(0.1, batch_size=100, cost=Dropout(),
                       monitoring_dataset=monitoring, termination_criterion=termination)
 
-  save_path = "train_best.pkl"
+  save_path = "train_best_pie.pkl"
 
   train_job = Train(train, model, algorithm, extensions=extensions, save_path="trainpie.pkl", save_freq=1)
   train_job.main_loop()
@@ -123,6 +129,23 @@ def MultiPIE():
 
   y = np.where(test.get_targets())[1]
   print 'accuracy', (y==yhat).sum() / y.size
+
+def getMultiPIEindices():
+  x, y = readMultiPIE()
+  l = len(x)
+
+  kf = cross_validation.KFold(n=l, n_folds=5)
+  for train, test in kf:
+    break
+
+  allIndices = shuffle(np.arange(l))
+
+  testIndices = allIndices[test]
+  stopValidation = len(train)/10
+  validationIndices = allIndices[train][0: stopValidation]
+  trainIndices = allIndices[train][stopValidation: ]
+
+  return trainIndices, validationIndices, testIndices
 
 if __name__ == '__main__':
   MNISTmain()
