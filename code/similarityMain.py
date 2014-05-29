4bad69d343a244ca1adb87cfae444358fb264811
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-05-29 13:17:17 +0100

    clean up

--- code/similarityMain.py
+++ code/similarityMain.py
@@ -291,11 +291,6 @@ def similarityCV():
      splitDataMultiPIESubject(instanceToPairRatio=2)
 
   if args.relu:
-    # TODO: params for relu
-    # params = [(0.0001, 0.01), (0.0001, 0.005), (0.001, 0.01), (0.001, 0.005)]
-    # TODO: try this
-    # params = [(0.001, 0.01), (0.001, 0.005), (0.01, 0.01), (0.01, 0.005)]
-
     visibleActivationFunction = Identity()
     hiddenActivationFunction = RectifiedNoisy()
     # IMPORTANT: SCALE THE DATA IF YOU USE GAUSSIAN VISIBlE UNITS
