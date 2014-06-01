eed163e2e29619141507e1a0d9152ad69e6a0ced
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-06-01 14:11:25 +0100

    showing some images

--- code/emotions.py
+++ code/emotions.py
@@ -1329,6 +1329,10 @@ def missingDataTestFromTrainedNet():
     for j in xrange(3):
       dictSquares[(i,j)] = []
 
+  for i in xrange(10):
+    plt.imshow(vectorToImage(testData[i], SMALL_SIZE), cmap=plt.cm.gray, interpolation="nearest")
+    plt.show()
+
   probs, predicted = net.classify(testData)
 
   actualLabels = testLabels
