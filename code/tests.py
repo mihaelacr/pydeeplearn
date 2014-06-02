598d0482ae52ecaa4ac40adde899ad2bd79abfb1
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-06-02 14:02:18 +0100

    fix isinstance call

--- code/tests.py
+++ code/tests.py
@@ -100,7 +100,7 @@ def testPicklingDBN():
 
   for key in initialDict:
     assert key in afterDict
-    if isinstace(initialDict[key], (np.ndarray, np.generic)):
+    if isinstance(initialDict[key], (np.ndarray, np.generic)):
       assert np.arrays_equal(initialDict[key], afterDict[key])
     else:
       assert initialDict[key] == afterDict[key]
