a3cee8525278e686241e1c4b3157720d83ed1b65
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-06-13 21:07:14 +0100

    fix the network argument

--- code/similarityMain.py
+++ code/similarityMain.py
@@ -697,7 +697,7 @@ def similaritySameSubjectDifferentEmotionsValues():
                                       rbmDropoutHid=1.0,
                                       rbmDropoutVis=1.0,
                                       rmsprop=False,
-                                      momentumFactorForLearningRate=True,
+                                      momentumFactorForLearningRateRBM=True,
                                       trainingEpochsRBM=args.rbmepochs,
                                       nesterovRbm=True,
                                       sparsityConstraint=args.sparsity,
