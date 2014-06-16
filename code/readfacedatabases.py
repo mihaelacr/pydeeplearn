3af1a2a93a8b30c7310d837253686fbe4aefe66b
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-06-16 21:44:20 +0100

    more print

--- code/readfacedatabases.py
+++ code/readfacedatabases.py
@@ -744,6 +744,7 @@ def readCropEqualize(path, extension, crop, doRecognition, equalize=False,
         print fullPath
         print shortPath
         img = cv2.imread(fullPath, 0)
+        print img == None
 
         face = facedetection.cropFace(img)
 
