c987293b4f301171ebb63690100cf2240c594e39
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-06-17 01:28:57 +0100

    huge parameter check for similarity

--- code/emotions.py
+++ code/emotions.py
@@ -421,8 +421,7 @@ def deepbeliefKanade(big=False):
 
 def buildUnsupervisedDataSetForKanadeLabelled():
   # return readJaffe(args.crop, args.facedetection, equalize=args.equalize)
-  return np.vstack((readAttData(equalize=args.equalize),
-                    readJaffe(args.crop, args.facedetection, equalize=args.equalize),
+  return np.vstack((readJaffe(args.crop, args.facedetection, equalize=args.equalize),
                     readNottingham(args.crop, args.facedetection, equalize=args.equalize)))
                     # readAberdeen(args.crop, args.facedetection, equalize=args.equalize)))
     # readCroppedYale(),
