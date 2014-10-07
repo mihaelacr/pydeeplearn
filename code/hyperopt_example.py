7a587f73393f61efad7ce81c78afbe4a7134a1a0
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2014-10-06 23:43:30 -0300

    Update hyperopt_example.py
    Update hyperopt_example.py
    
    Update hyperopt_example.py
    
    Update hyperopt_example.py
    
    Update hyperopt_example.py

--- code/hyperopt_example.py
+++ code/hyperopt_example.py
@@ -1,25 +1,53 @@
 # This module creates an optimization of hyper-parameters of a DBN using hyperopt library (https://github.com/hyperopt/hyperopt).
 #  
 # 
-
+import argparse
+import matplotlib.pyplot as plt
 import numpy as np
-import hyperopt
+import cPickle as pickle
 
 from hyperopt import hp, fmin, tpe
+
 from sklearn import cross_validation
+
 from lib import deepbelief as db
-from read import readmnist
+from lib import restrictedBoltzmannMachine as rbm
+from lib.common import *
 from lib.activationfunctions import *
+from lib.trainingoptions import *
 
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import precision_recall_fscore_support
 
-def objective(args):
-    activationFunction = Sigmoid()
-    unsupervisedLearningRate = 0.01
-    supervisedLearningRate = 0.05
-    momentumMax = 0.95    
-    nrLayers = 5
-    layerSizes = 1000
-    
+theanoFloat  = theano.config.floatX
+
+train_data = []
+train_label = []
+
+parser = argparse.ArgumentParser(description='digit recognition')
+parser.add_argument('--trainSize', type=int, default=10000,
+                    help='the number of tranining cases to be considered')
+parser.add_argument('--path',dest='path', default="MNIST", help="the path to the MNIST files")
+args = parser.parse_args()
+
+# define an objective function
+def objective(nrLayers,layerSizes,
+              unsupervisedLearningRate,
+              supervisedLearningRate,
+              momentumMax,
+              visibleDropout,
+              ):
+            
+            
+    activationFunction = Sigmoid()  
+    rbmHiddenDropout = 1.0
+    rbmVisibleDropout = 1.0    
+    weightDecayL1 = 0
+    weightDecayL2 = 0 
+    preTrainEpochs = 1
+    maxEpochs = 100
+    hiddenDropout = 0.5
     hiddenLayers = []
     for i in range(0,nrLayers-2):
         hiddenLayers.append(layerSizes)
@@ -27,8 +55,8 @@ def objective(args):
     dbnLayers.insert(0,784)
     dbnLayers.append(10)        
     
-    nrFold = 5
-    training =  len(labels)
+    nrFolds = 5
+    training =  len(vectorLabels)
     kf = cross_validation.kFold(n=training, n_folds=nrFolds)
     foldError =[]
     for training, testing in kf:
@@ -44,28 +72,54 @@ def objective(args):
                      nesterovMomentum=True,
                      rbmNesterovMomentum=True,
                      rmsprop=True,
-                     hiddenDropout=0.5,
-                     visibleDropout=0.8,
-                     rbmHiddenDropout=1.0,
-                     rbmVisibleDropout=1.0,
-                     weightDecayL1=0,
-                     weightDecayL2=0,
-                     preTrainEpochs=100)
-        net.train(trainingData, vectorLabels,
-                  maxEpochs=100, validation=True)  
+                     hiddenDropout=hiddenDropout,
+                     visibleDropout=visibleDropout,
+                     rbmHiddenDropout=rbmHiddenDropout,
+                     rbmVisibleDropout=rbmHiddenDropout,
+                     weightDecayL1=weightDecayL1,
+                     weightDecayL2=weightDecayL2,
+                     preTrainEpochs= preTrainEpochs)
+        net.train(train_data[training], train_label[training],
+                  maxEpochs=maxEpochs, validation=False)
                   
+        proabilities, predicted = net.classify(train_data[testing])
+        testLabels = train_label[testing]
+        
+        UACROC.append(roc_auc_score(testLabels, proabilities))            
+    
+    return 1-np.mean(UACROC)
 
 
-space = ( 
-	hp.qloguniform( 'l1_dim', log( 10 ), log( 1000 ), 1 ), 
-	hp.qloguniform( 'l2_dim', log( 10 ), log( 1000 ), 1 ),
-	hp.loguniform( 'learning_rate', log( 1e-5 ), log( 1e-2 )),
-	hp.uniform( 'momentum', 0.5, 0.99 ),
-	hp.uniform( 'l1_dropout', 0.1, 0.9 ),
-	hp.uniform( 'decay_factor', 1 + 1e-3, 1 + 1e-1 )
-)
+def main():
+    from numpy import genfromtxt    
+    import random
+    print "FIXING RANDOMNESS"
+    random.seed(6)
+    np.random.seed(6)    
+    
+    training = args.trainSizeactivationFunction
 
+    trainVectors, trainLabels =\
+        readmnist.read(0, training, bTrain=True, path=args.path)
 
-best = fmin( run_test, space, algo = tpe.suggest, max_evals = 50 )
-
-print best
+    trainVectors, trainLabels = shuffle(trainVectors, trainLabels)    
+    trainingScaledVectors = trainVectors / 255.0          
+    global train_data = trainingScaledVectors
+    global train_label = trainLabels   
+  
+ # define a search space
+    space = ( 
+      hp.choice('nrLayers', [2,3])
+      hp.choice('layerSizes', [500,600,700,800,900,1000,1100,1200])
+      hp.loguniform( 'unsupervisedLearningRate', log( 1e-5 ), log( 1e-1)),
+      hp.loguniform( 'supervisedLearningRate', log( 1e-5 ), log( 1e-1 )),
+      hp.uniform( 'momentumMax', 0.5, 0.99 ),
+      hp.uniform( 'visibleDropout',  0.1, 0.9 ),
+    )
+# minimize the objective over the space
+    best = fmin( objective, space, algo = tpe.suggest, max_evals = 50 )
+	
+    print best
+  
+if __name__ == '__main__':
+  main()
