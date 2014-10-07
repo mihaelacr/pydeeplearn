311e571aa4c864ff4e4fb536099958ca5ddd3a20
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2014-10-06 23:41:42 -0300

    Update hyperopt_example.py

--- code/hyperopt_example.py
+++ code/hyperopt_example.py
@@ -12,6 +12,49 @@ from read import readmnist
 from lib.activationfunctions import *
 
 
+def objective(args):
+    activationFunction = Sigmoid()
+    unsupervisedLearningRate = 0.01
+    supervisedLearningRate = 0.05
+    momentumMax = 0.95    
+    nrLayers = 5
+    layerSizes = 1000
+    
+    hiddenLayers = []
+    for i in range(0,nrLayers-2):
+        hiddenLayers.append(layerSizes)
+    dbnLayers=hiddenLayers
+    dbnLayers.insert(0,784)
+    dbnLayers.append(10)        
+    
+    nrFold = 5
+    training =  len(labels)
+    kf = cross_validation.kFold(n=training, n_folds=nrFolds)
+    foldError =[]
+    for training, testing in kf:
+               
+        net = db.DBN(nrLayers, dbnLayers,
+                     binary=False,
+                     unsupervisedLearningRate=unsupervisedLearningRate,
+                     supervisedLearningRate=supervisedLearningRate,
+                     momentumMax=momentumMax,
+                     activationFunction=activationFunction,
+                     rbmActivationFunctionVisible=activationFunction,
+                     rbmActivationFunctionHidden=activationFunction,
+                     nesterovMomentum=True,
+                     rbmNesterovMomentum=True,
+                     rmsprop=True,
+                     hiddenDropout=0.5,
+                     visibleDropout=0.8,
+                     rbmHiddenDropout=1.0,
+                     rbmVisibleDropout=1.0,
+                     weightDecayL1=0,
+                     weightDecayL2=0,
+                     preTrainEpochs=100)
+        net.train(trainingData, vectorLabels,
+                  maxEpochs=100, validation=True)  
+                  
+
 
 space = ( 
 	hp.qloguniform( 'l1_dim', log( 10 ), log( 1000 ), 1 ), 
