ff1e3be1cd47f1655c57f7e4e38dc8fa74184d35
Mihaela Rosca <mihaelacrosca@gmail.com>
2015-03-01 20:38:00 +0100

    scope for forwardPass in the adversarial input

--- code/lib/deepbelief.py
+++ code/lib/deepbelief.py
@@ -96,7 +96,7 @@ class MiniBatchTrainer(BatchTrainer):
       error = T.sum(self.costFun(self.input, self.inputLabels))
       grad_error = T.grad(error, self.input)
       adversarial_input = self.input + self.adversarial_epsilon * T.sgn(grad_error)
-      self.adversarial_output = forwardPass(adversarial_input)
+      self.adversarial_output = self.forwardPass(adversarial_input)
 
   def forwardPass(x):
     # Sample from the visible layer
