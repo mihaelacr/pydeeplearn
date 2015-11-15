2e4c02fcaf0097f4d5a63917db27ca67884b6a69
Mihaela Rosca <mihaelacrosca@gmail.com>
2015-11-15 18:51:58 +0100

    Manually import matplotlib

--- code/PCA.py
+++ code/PCA.py
@@ -15,8 +15,17 @@ from scipy import misc
 # Import all common functions
 from lib.common import *
 
-import lib.matplotlib_plt_import
-lib.matplotlib_plt_import.ImportMatplotlibPlot()
+import matplotlib
+import os
+havedisplay = "DISPLAY" in os.environ
+if not havedisplay:
+  exitval = os.system('python -c "import matplotlib.pyplot as plt; plt.figure()"')
+  havedisplay = (exitval == 0)
+if havedisplay:
+  import matplotlib.pyplot as plt
+else:
+  matplotlib.use('Agg')
+  import matplotlib.pyplot as plt
 
 # The directory path to the images
 PICTURE_PATH = "/pics/cambrdige_pics/"
